408e3a5647dcc0c78768474358bface715ca3004
gbotas <gbotas@uchicago.edu>
2025-04-28 19:27:37 +0000

    thirdcommit_post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,15 @@ class HomeController < ApplicationController
 def homepage
   render({ :template => "home_templates/home"})
 end 
+
+
+
+def process_inputs
+
+  @the_image = params.fetch("image_param")
+  
+  @the_description = params.fetch("description_param")
+  
+  render({:template => "home_templates/results"})
+end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
 
   # Solutions below. Don't peek until you try it yourself and get stuck!
 
-  get("/blank_form", { :controller => "home", :action => "homepage"})
+
 
   get("/solutions/blank_form", { :controller => "solutions", :action => "display_form" })
   
   post("/solutions/process_form", { :controller => "solutions", :action => "process_inputs" })
+
+  get("/blank_form", { :controller => "home", :action => "homepage"})
+
+  post("/process_form_inputs", { :controller => "home", :action => "process_inputs"})
 
   
 end
