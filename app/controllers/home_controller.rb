f96c6fc1d5fddc5ab78a5245dbb6a7546b5a5e3a
gbotas <gbotas@uchicago.edu>
2025-04-28 19:38:22 +0000

    fourthcommit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,9 +8,9 @@ end
 
 def process_inputs
 
-  @the_image = params.fetch("image_param")
-  
-  @the_description = params.fetch("description_param")
+  @the_image = params.fetch("image_param", "")
+
+  @the_description = params.fetch("description_param", "")
   
   render({:template => "home_templates/results"})
 end 
